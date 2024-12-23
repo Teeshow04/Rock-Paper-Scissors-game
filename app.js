8aa76b10c975425039b466b6350373cfe1782689
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2024-12-23 17:12:13 +0100

    implement the page ans styling

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
 
-const computerChoiceDisplay = document.getElementById("computer-choice")
-const userChoiceDisplay = document.getElementById("user-choice")
-const  resultDisplay = document.getElementById("result")
-const possibleChoices = document.querySelectorAll("button")
-let userChoice
-let computerChoice
-
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML =userChoice
-    generateComputerrChoice()
-}))
-
-function generateComputerrChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) +1
-}
