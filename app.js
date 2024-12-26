c6474dc0b18bcc124351767e31352a5e60eb70ca
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2024-12-26 02:48:52 +0100

    updated the page

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const computerChoiceDisplay = document.getElementById("computer-choice")
+const userChoiceDisplay = document.getElementById("user-choice")
+const resultDisplay = document.getElementById("result")
+const possibleChoices = document.querySelectorAll("button")
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
 
